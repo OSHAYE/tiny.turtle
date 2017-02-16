6003b4b7dfd759fd7cf52d64d8814f97a2127008
Shaye <omercadowatson2017@bklawtech.com>
2017-02-16 20:51:39 +0000

    tiny turtle

--- tiny-turtle/script.js
+++ tiny-turtle/script.js
@@ -1,2 +1,23 @@
 // JavaScript File
  TinyTurtle.apply(window);
+ /* global TinyTurtle */
+  /*global stamp*/
+  /*global forward*/
+  /*global right*/
+  forward(70);
+  right(90);
+  forward(60);
+  right(90);
+  forward(60);
+  left(90);
+  forward(50);
+  right(105);
+  forward(60);
+  right(120);
+  forward(60);
+  left(45);
+  forward(60);
+  right(90);
+  forward(5);
+  right(90);
+  stamp();
